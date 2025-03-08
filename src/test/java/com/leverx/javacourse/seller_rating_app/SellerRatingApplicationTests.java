1d82b9394ab7d163fbb8aa6ecc8a19f0eec316f9
Mikita Kavaliou <neket2305@gmail.com>
2025-03-08 23:47:02 +0300

    Spring Boot project created

---
+++ src/test/java/com/leverx/javacourse/seller_rating_app/SellerRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leverx.javacourse.seller_rating_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellerRatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
